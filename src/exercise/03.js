1900427e547505b6b82136690a16c0f62210fc45
Nick Ang <nickangtc@gmail.com>
2022-07-29 12:33:44 +0200

    03 - use context API instead of map for nested implicit state sharing

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -4,8 +4,9 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
-// 🐨 create your ToggleContext context here
-// 📜 https://reactjs.org/docs/context.html#reactcreatecontext
+// Remember: Context lets you wrap any number of nested children
+// and allows those children to grab values directly from the parent
+const ToggleContext = React.createContext()
 
 function Toggle({children}) {
   const [on, setOn] = React.useState(false)
@@ -13,31 +14,31 @@ function Toggle({children}) {
 
   // 🐨 remove all this 💣 and instead return <ToggleContext.Provider> where
   // the value is an object that has `on` and `toggle` on it.
-  return React.Children.map(children, child => {
-    return typeof child.type === 'string'
-      ? child
-      : React.cloneElement(child, {on, toggle})
-  })
+  return (
+    <ToggleContext.Provider value={{on, toggle}}>
+      {children}
+    </ToggleContext.Provider>
+  )
 }
 
-// 🐨 we'll still get the children from props (as it's passed to us by the
-// developers using our component), but we'll get `on` implicitly from
-// ToggleContext now
-// 🦉 You can create a helper method to retrieve the context here. Thanks to that,
-// your context won't be exposed to the user
-// 💰 `const context = React.useContext(ToggleContext)`
-// 📜 https://reactjs.org/docs/hooks-reference.html#usecontext
-function ToggleOn({on, children}) {
+// helper function which is really a custom hook
+// designed to get the context
+function useToggle() {
+  return React.useContext(ToggleContext)
+}
+
+function ToggleOn({children}) {
+  const {on} = useToggle()
   return on ? children : null
 }
 
-// 🐨 do the same thing to this that you did to the ToggleOn component
-function ToggleOff({on, children}) {
+function ToggleOff({children}) {
+  const {on} = useToggle()
   return on ? null : children
 }
 
-// 🐨 get `on` and `toggle` from the ToggleContext with `useContext`
-function ToggleButton({on, toggle, ...props}) {
+function ToggleButton({...props}) {
+  const {on, toggle} = useToggle()
   return <Switch on={on} onClick={toggle} {...props} />
 }
 
@@ -48,6 +49,8 @@ function App() {
         <ToggleOn>The button is on</ToggleOn>
         <ToggleOff>The button is off</ToggleOff>
         <div>
+          {/* The problem is ToggleButton, being nested, doesn't get implicit state using React.cloneElement */}
+          {/* The solution is to use React Context API */}
           <ToggleButton />
         </div>
       </Toggle>
